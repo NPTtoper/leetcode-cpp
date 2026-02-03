1c05b3fdc46c41c53f929d299f93bcbe416a4682
NPTtoper <galaktionovn@gmail.com>
2026-02-03 21:14:35 +0300

    refactor: New solution

--- hard/1_MedianOfTwoSortedArrays.cpp
+++ hard/1_MedianOfTwoSortedArrays.cpp
@@ -21,7 +21,10 @@ Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 using namespace std;
 
-class Solution {
+/*
+Первая попытка P.s. Я - балда, поменыял код, а в ком. вставил новый. В первый раз пытался решить
+двумя указателями с начал массивов
+class Solution  {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
@@ -69,11 +72,55 @@ public:
         
         return 0.0;
     }
+}; */
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2;
+        int low = 0, high = n1;
+        
+        while (low <= high) {
+            int mid1 = (low + high) >> 1; 
+            int mid2 = left - mid1;
+            
+            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1)
+                    return max(l1, l2);
+                else
+                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+            }
+            else if (l1 > r2) {
+                high = mid1 - 1;
+            }
+            else {
+                low = mid1 + 1;
+            }
+        }
+        
+        return 0; 
+    }
 };
 
 int main() {
     Solution temp;
-    vector<int> tempVec1{1,5,7}, tempVec2{1,4,7,9,12};
+    vector<int> tempVec1{1,5,7}, tempVec2{4,7,9,12};
 
     cout << temp.findMedianSortedArrays(tempVec1, tempVec2) << endl; 
 
