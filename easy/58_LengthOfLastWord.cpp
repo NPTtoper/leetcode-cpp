01bac19a16e1d79f28bb96cc39166c32db9dc5aa
NPTtoper <galaktionovn@gmail.com>
2026-02-06 18:17:16 +0300

    task: new easy task

---
+++ easy/58_LengthOfLastWord.cpp
@@ -0,0 +1,48 @@
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+Example 1:
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+*/
+
+#include <iostream>
+
+class Solution {
+public:
+    int lengthOfLastWord(const std::string s) {
+
+        int i = s.length() - 1;
+
+        while (i >= 0 && s[i] == ' ') i--;
+        
+        int end = i;
+
+        while (i >= 0 && s[i] != ' ') i--;
+
+        return end - i;
+    }
+};
+
+
+int main() {
+
+    Solution temp;
+    
+    std::cout << temp.lengthOfLastWord("");
+
+    return 0;
+}
